8e54d542a1354ace6a13c06f0e20d85a5be71ef3
Simon <comficker@gmail.com>
2026-02-08 17:37:36 +0700

    optimize SEO

--- app/plugins/node.client.ts
+++
@@ -1,8 +0,0 @@
-import { Buffer } from "buffer";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    // @ts-ignore
-    globalThis.Buffer = Buffer
-    // @ts-ignore
-    window.Buffer = Buffer
-})
